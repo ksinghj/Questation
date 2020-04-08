761cbb843f58ae9f5182f71645b52c567cd26e96
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-04-08 16:53:08 +0100

    update client

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <BrowserRouter>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
