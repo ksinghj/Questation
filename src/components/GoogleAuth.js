d541a8fdc2275dfc85801c787f5d7501ef088729
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-04-09 10:49:17 +0100

    Update GoogleAuth.js

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "142108740797-l349m9i6u24jjafqs34ooo6vndehm6ma.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
   render() {
     return <div>GoogleAuth</div>;
   }
