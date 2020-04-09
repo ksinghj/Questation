bba80c87629d22eadefec75ba05b5ae105a59a30
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-04-09 12:44:26 +0100

    add redux to GoogleAuth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
+// This file uses Semantic UI classNames
 class GoogleAuth extends React.Component {
-  state = { isSignedIn: null };
-
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
       window.gapi.client
@@ -13,14 +13,18 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() }); // check isSignedIn
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
   };
 
   onSignInClick = () => {
@@ -32,7 +36,7 @@ class GoogleAuth extends React.Component {
   };
 
   renderAuthButton = () => {
-    switch (this.state.isSignedIn) {
+    switch (this.props.isSignedIn) {
       case null:
         return null;
       case true:
@@ -61,6 +65,13 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
 
-// TODO: Add this file to HELPERS when complete for other apps
+export default connect(mapStateToProps, {
+  signOut,
+  signIn,
+})(GoogleAuth);
+
+// TODO: Add this file to HELPERS when complete for other apps REDUX VERSION
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
+import authReducer from "./authReducer";
 
 export default combineReducers({
-  replaceMe: () => {
-    return "replace this reducer";
-  },
+  auth: authReducer,
 });
