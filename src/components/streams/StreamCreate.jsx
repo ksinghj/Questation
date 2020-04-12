769812c6ea69948cb6fe7d75e7410f1a5e6152fe
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-04-12 14:00:48 +0100

    Update StreamCreate.jsx

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-
+// Most of this file is standard redux-form syntax
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     // Destructured formProps.input
+    console.log(meta);
     return (
       <div className="field">
         <label>{label}</label>
@@ -43,17 +44,16 @@ class StreamCreate extends React.Component {
 
 const validate = formValues => {
   const errors = {};
-
-  switch (formValues) {
-    case !formValues.title:
-      return (errors.title = "Enter a title for your stream");
-    case !formValues.description:
-      return (errors.description = "Your stream needs a description!");
-    default:
-      return errors;
+  if (!formValues.title) {
+    return (errors.title = "Enter a title for yourr stream");
   }
+  if (!formValues.description) {
+    return (errors.description = "Your stream needs a description!");
+  }
+  return errors;
 };
 
 export default reduxForm({
   form: "streamCreate",
+  validate,
 })(StreamCreate);
