567b68b11ad27bf6436222bc4b4c7b36a10c0c7c
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-04-12 13:55:50 +0100

    Update StreamCreate.jsx

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -7,15 +7,21 @@ class StreamCreate extends React.Component {
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
+        <input required {...input} />
       </div>
     );
   }
 
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
   render() {
     return (
       <div>
-        <form className="ui form">
+        <form
+          className="ui form"
+          onSubmit={this.props.handleSubmit(this.onSubmit)}>
           <Field
             name="title"
             component={this.renderInput}
@@ -26,12 +32,27 @@ class StreamCreate extends React.Component {
             component={this.renderInput}
             label="Enter description"
           />
-          <button type="submit">Submit</button>
+          <button type="submit" className="ui button primary">
+            Submit
+          </button>
         </form>
       </div>
     );
   }
 }
+
+const validate = formValues => {
+  const errors = {};
+
+  switch (formValues) {
+    case !formValues.title:
+      return (errors.title = "Enter a title for your stream");
+    case !formValues.description:
+      return (errors.description = "Your stream needs a description!");
+    default:
+      return errors;
+  }
+};
 
 export default reduxForm({
   form: "streamCreate",
