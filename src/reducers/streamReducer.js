61e36343125a3f19bb5a8fe7688cc53200bfc5a5
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-04-14 11:23:25 +0100

    Create streamReducer.js

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,20 @@
+import {
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+  CREATE_STREAM,
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
