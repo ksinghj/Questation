e536edc427d74a9be1f0459beb8c545e034ddc25
kartar singh <44725786+KJabandaa10@users.noreply.github.com>
2020-04-15 20:47:41 +0100

    finish section with patch request

--- src/actions/index.js
+++ src/actions/index.js
@@ -44,9 +44,10 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const res = await streams.put(`/streams/${id}`, formValues);
+  const res = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: res.data });
+  history.push("/");
 };
 
 export const deleteStream = id => async dispatch => {
