401bfcae5b9157ec725d94fa03f72faf8792d2e2
kartar singh <44725786+KJabandaa10@users.noreply.github.com>
2020-04-15 20:22:37 +0100

    form for edit

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,32 +1,9 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
 import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError = ({ error, touched }) => {
-    if (error && touched) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  };
-
-  renderInput = ({ input, label, meta }) => {
-    // Destructured formProps.input
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`; // string interpolation with ternerary operator
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
   onSubmit = formValues => {
     this.props.createStream(formValues);
   };
@@ -34,43 +11,11 @@ class StreamCreate extends React.Component {
   render() {
     return (
       <div>
-        <form
-          className="ui form error"
-          onSubmit={this.props.handleSubmit(this.onSubmit)}>
-          <Field
-            name="title"
-            component={this.renderInput}
-            label="Enter title"
-          />
-          <Field
-            name="description"
-            component={this.renderInput}
-            label="Enter description"
-          />
-          <button type="submit" className="ui button primary">
-            Submit
-          </button>
-        </form>
+        <h3>Create a stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
       </div>
     );
   }
 }
 
-const validate = formValues => {
-  const errors = {};
-
-  if (!formValues.title) {
-    errors.title = "Enter a title for your stream";
-  }
-  if (!formValues.description) {
-    errors.description = "Your stream needs a description!";
-  }
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: "streamCreate",
-  validate,
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
--- src/components/streams/StreamEdit.jsx
+++ src/components/streams/StreamEdit.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
   render() {
-    const { stream } = this.props;
-    if (!stream) {
-      return <div>Getting stream...</div>;
-    }
-    return <div>{stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
