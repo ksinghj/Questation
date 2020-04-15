508d85d06dfd0c00f5576cb5b02bfcccf4ea3432
kartar singh <44725786+KJabandaa10@users.noreply.github.com>
2020-04-15 19:49:13 +0100

    Update StreamEdit.jsx

--- src/components/streams/StreamEdit.jsx
+++ src/components/streams/StreamEdit.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    const { stream } = this.props;
+    if (!stream) {
+      return <div>Getting stream...</div>;
+    }
+    return <div>{stream.title}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
-
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
