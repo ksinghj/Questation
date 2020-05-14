eba18b07783e39cc48404ff5a279d24a40ecdfe5
Kartar Singh <kartarjabanda@hotmail.co.uk>
2020-05-14 11:51:58 +0100

    create 5qs

--- src/components/pages/QuestionCreate.jsx
+++ src/components/pages/QuestionCreate.jsx
@@ -1,59 +1,58 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createQuestionSheet } from "../../actions";
-import QuestionForm from "./QuestionForm";
 
 class QuestionCreate extends React.Component {
-  state = {
-    questions: [1],
-    formValues: "",
-  };
+  state = { input1: "", input2: "", input3: "", input4: "", input5: "" };
 
-  addQuestion = e => {
+  onFormSubmit = e => {
     e.preventDefault();
-    console.log("form submitted");
-    this.props.createQuestionSheet(this.state.formValues);
-    let length = this.state.questions.length + 1;
-    this.setState({ questions: this.state.questions.concat(length + 1) });
+    console.log(this.state);
+    // this.props.createQuestionSheet(this.state);
   };
 
-  renderQuestions = () => {
-    return this.state.questions.map((item, index) => {
-      return (
-        <QuestionForm
-          onInputChange={this.onInputChange}
-          key={item}
-          arrayIndex={index}
-        />
-      );
-    });
+  handeInput1Change = e => {
+    this.setState({ input1: e.target.value });
   };
 
-  onQuestionFormSubmit = e => {
-    e.preventDefault();
-    console.log("form submitted");
-    this.props.createQuestionSheet(this.state.formValues); // caLL with this.state.formValues
+  handeInput2Change = e => {
+    this.setState({ input2: e.target.value });
   };
 
-  onInputChange = e => {
-    this.setState({
-      formValues: e.target.value,
-    });
-    console.log(this.state.formValues);
+  handeInput3Change = e => {
+    this.setState({ input3: e.target.value });
+  };
+
+  handeInput4Change = e => {
+    this.setState({ input4: e.target.value });
+  };
+
+  handeInput5Change = e => {
+    this.setState({ input5: e.target.value });
   };
 
   render() {
     return (
       <div>
-        <form onSubmit={this.onQuestionFormSubmit}>
-          {this.renderQuestions()}
-          <button
-            type="button"
-            className="ui button"
-            onClick={this.addQuestion}
-          >
-            Add a question
-          </button>
+        <form
+          onSubmit={this.onFormSubmit}
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <div className="ui input">
+            <input onChange={this.handeInput1Change} id="1" />
+          </div>
+          <div className="ui input">
+            <input onChange={this.handeInput2Change} id="2" />
+          </div>
+          <div className="ui input">
+            <input onChange={this.handeInput3Change} id="3" />
+          </div>
+          <div className="ui input">
+            <input onChange={this.handeInput4Change} id="4" />
+          </div>
+          <div className="ui input">
+            <input onChange={this.handeInput5Change} id="5" />
+          </div>
           <button className="ui button green" type="submit">
             Continue
           </button>
