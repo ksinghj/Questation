16d71411898aaed0f8763715825c844d7c4b3e58
Kartar Singh <kartarjabanda@hotmail.co.uk>
2020-05-17 17:42:17 +0100

    student number component state

--- src/components/GenerateSheets.jsx
+++ src/components/GenerateSheets.jsx
@@ -4,13 +4,33 @@ import "../styles/misc/button.css";
 import "../styles/GenerateSheets/index.css";
 
 class GenerateSheets extends React.Component {
+  state = { students: 0, notZero: false };
+
+  handleStudentNumberEnter = e => {
+    this.setState({ students: e.target.value });
+    setTimeout(() => {
+      if (this.state.students !== 0) {
+        this.setState({ notZero: true });
+      }
+    }, 750);
+  };
+
   render() {
     return (
       <div className="generatesheets__container">
-        <p>Generate unique sheets for </p>
-        <button className="button generatesheets__button" type="submit">
-          Generate sheets
-        </button>
+        <h3>Important:</h3>
+        <form>
+          <label>Enter number of students in your class:</label>
+          <input onChange={this.handleStudentNumberEnter} />
+          <p>Generate unique sheets for {this.state.students} students.</p>
+          <button
+            className="button generatesheets__button"
+            type="submit"
+            disabled={this.state.notZero ? false : true}
+          >
+            Generate sheets
+          </button>
+        </form>
       </div>
     );
   }
