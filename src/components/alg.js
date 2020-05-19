2ff99e0b1299010f1cef98fa131b6fc12903300a
ksinghj <kartarjabanda@hotmail.co.uk>
2020-05-19 10:51:02 +0100

    add comments for plan of action

--- src/components/alg.js
+++ src/components/alg.js
@@ -3,6 +3,8 @@
 //   console.log(`Result: ` + result.match(/\d+/g));
 // };
 
+// See board for info
+
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 export const extractNums = input => {
@@ -15,3 +17,35 @@ export const extractNums = input => {
   // if they match, replace it with a random number from numbers
   // if !match, move on to next idx
 };
+
+// need randomNum function
+// takes number(string) as input
+// outputs a random idx from numbers ^ array
+
+// ???
+// math.evaluate()
+// ? store in question: answer pairs obj
+// so eventually we have:
+// let students = {
+//   student1: {
+//     question1: answer,
+//     question2: answer,
+//     question3: answer,
+//     question4: answer,
+//     question5: answer
+//   },
+//   student2: {
+//     question1: answer,
+//     question2: answer,
+//     question3: answer,
+//     question4: answer,
+//     question5: answer
+//   },
+//   student3: {
+//     question1: answer,
+//     question2: answer,
+//     question3: answer,
+//     question4: answer,
+//     question5: answer
+//   }
+// };
