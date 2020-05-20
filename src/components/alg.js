c8c53da15be19a18bdbd13e648871f31e0f196fa
ksinghj <kartarjabanda@hotmail.co.uk>
2020-05-20 12:18:24 +0100

    alg currentIdx

--- src/components/alg.js
+++ src/components/alg.js
@@ -5,22 +5,26 @@
 
 // See board for info
 
+// random number()
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
 
 export const extractNums = input => {
-  // console.log(`Extracted numbers: ` + input.match(/\d+/g));
-  // input.match(/\d+/g));
   let extractedToArr = input.split("");
-  console.log(extractedToArr);
-  let replaceNums = extractedToArr.map(idx => {});
-  // compare idx to every value in numbers
-  // if they match, replace it with a random number from numbers
-  // if !match, move on to next idx
-};
+  console.log(`extractedToArr = `, extractedToArr);
+  let replaceNums = extractedToArr.map(currentIdx => {
+    if (numbers.includes(currentIdx)) {
+      currentIdx = randomNumber;
+      console.log(`New currentIdx = `, currentIdx);
+    }
+  });
 
-// need randomNum function
-// takes number(string) as input
-// outputs a random idx from numbers ^ array
+  console.log(`replaceNums = `, replaceNums);
+
+  // compare currentIdx to every value in numbers
+  // if they match, replace it with a random number from numbers
+  // if !match, move on to next currentIdx
+};
 
 // ???
 // math.evaluate()
