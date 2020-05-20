7c5187c2a782d759502726c5e34adb5b9964d7ff
ksinghj <kartarjabanda@hotmail.co.uk>
2020-05-20 13:48:06 +0100

    Update GenerateSheets.jsx

--- src/components/GenerateSheets.jsx
+++ src/components/GenerateSheets.jsx
@@ -5,8 +5,6 @@ import { enterStudents } from "../actions";
 import "../styles/misc/button.css";
 import "../styles/GenerateSheets/index.css";
 
-import { replaceNums } from "./alg";
-
 class GenerateSheets extends React.Component {
   state = { students: 0, notZero: false };
 
@@ -29,28 +27,6 @@ class GenerateSheets extends React.Component {
     if (this.state.notZero) {
       this.props.enterStudents(this.state.students);
     }
-
-    // need to replaceNums() for every student
-    // number to arr length function (helper)
-    const loopForEveryStudent = number => {
-      let numberAsArr = [];
-      let count = 0;
-      while (count < number) {
-        count++;
-        numberAsArr.push(count);
-      }
-
-      return numberAsArr.forEach(arrItem => {
-        // algorithm TODO: make alg run on every input for every student
-        replaceNums(this.props.questions.input1);
-        replaceNums(this.props.questions.input2);
-        replaceNums(this.props.questions.input3);
-        replaceNums(this.props.questions.input4);
-        replaceNums(this.props.questions.input5);
-      });
-    };
-
-    loopForEveryStudent(this.state.students);
   };
 
   render() {
