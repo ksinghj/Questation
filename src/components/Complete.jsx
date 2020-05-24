0fac0336016012c0549f933ced826eca232bbf5c
ksinghj <kartarjabanda@hotmail.co.uk>
2020-05-24 12:35:22 +0100

    Update Complete.jsx

--- src/components/Complete.jsx
+++ src/components/Complete.jsx
@@ -31,18 +31,3 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, {})(Complete);
-
-// evaluate expressions
-// math.evaluate('sqrt(3^2 + 4^2)')        // 5
-// math.evaluate('sqrt(-4)')               // 2i
-// math.evaluate('2 inch to cm')           // 5.08 cm
-// math.evaluate('cos(45 deg)')            // 0.7071067811865476
-
-// provide a scope
-// let scope = {
-//     a: 3,
-//     b: 4
-// }
-// math.evaluate('a * b', scope)           // 12
-// math.evaluate('c = 2.3 + 4.5', scope)   // 6.8
-// scope.c
