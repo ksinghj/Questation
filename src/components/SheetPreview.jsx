b9f7d6842a05c7efe5ddc22923d9a9ad3bee713a
ksinghj <kartarjabanda@hotmail.co.uk>
2020-05-24 16:10:07 +0100

    Revert "Revert "Update SheetPreview.jsx""
    This reverts commit 0c67b7259d2e2615f94854cc21dbff4b5defdcdd.

--- src/components/SheetPreview.jsx
+++ src/components/SheetPreview.jsx
@@ -2,29 +2,60 @@ import React from "react";
 import { connect } from "react-redux";
 import "../styles/SheetPreview/index.css";
 
-const SheetPreview = ({ data, sheet, answers }) => {
-  let arrToMap = Object.values(data);
+class SheetPreview extends React.Component {
+  // props: data, sheet, answers
 
-  return (
-    <div className="sheetpreview__container sheet">
-      <h4 className="sheetpreview__sheet-number">
-        {answers ? "Answers" : "Sheet"} {sheet}
-      </h4>
-      <ol className="sheetpreview__list">
-        {arrToMap.map(q => {
-          // TODO: new key to remove console error
-          return <li key={q}>{q}</li>;
-        })}
-      </ol>
-      <div className="sheetpreview__watermark">
-        <img
-          className="watermark__image"
-          src={require("../img/questation-logo.png")}
-          alt="Questation logo"
-        />
+  renderSheets = () => {
+    const { data, sheet, answers } = this.props;
+    let arrToMap = Object.values(data);
+    if (!answers) {
+      return (
+        <div className="sheetpreview__container sheet">
+          <h4 className="sheetpreview__sheet-number">
+            {answers ? "Answers" : "Sheet"} {sheet}
+          </h4>
+          <ol className="sheetpreview__list">
+            {arrToMap.map(q => {
+              // TODO: new key to remove console error
+              return <li key={q}>{q}</li>;
+            })}
+          </ol>
+          <div className="sheetpreview__watermark">
+            <img
+              className="watermark__image"
+              src={require("../img/questation-logo.png")}
+              alt="Questation logo"
+            />
+          </div>
+        </div>
+      );
+    }
+    // answers
+    return (
+      <div className="sheetpreview__container sheet">
+        <h4 className="sheetpreview__sheet-number">
+          {answers ? "Answers" : "Sheet"} {sheet}
+        </h4>
+        <ol className="sheetpreview__list">
+          {arrToMap.map(q => {
+            // TODO: new key to remove console error
+            return <li key={q}>{q}</li>;
+          })}
+        </ol>
+        <div className="sheetpreview__watermark">
+          <img
+            className="watermark__image"
+            src={require("../img/questation-logo.png")}
+            alt="Questation logo"
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+
+  render() {
+    return <div>{this.renderSheets(this.props.answers)}</div>;
+  }
+}
 
 export default connect(null, {})(SheetPreview);
