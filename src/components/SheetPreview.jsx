a7054ee3aa8601705f2ef2659a3b6e0db10dae5c
ksinghj <kartarjabanda@hotmail.co.uk>
2020-05-24 16:43:40 +0100

    ack to old SheetPreview

--- src/components/SheetPreview.jsx
+++ src/components/SheetPreview.jsx
@@ -5,31 +5,7 @@ import "../styles/SheetPreview/index.css";
 class SheetPreview extends React.Component {
   renderSheets = () => {
     const { data, sheet, answers } = this.props;
-    if (!answers) {
-      let arrToMap = Object.values(data);
-      return (
-        <div className="sheetpreview__container sheet">
-          <h4 className="sheetpreview__sheet-number">
-            {answers ? "Answers" : "Sheet"} {sheet}
-          </h4>
-          <ol className="sheetpreview__list">
-            {arrToMap.map(q => {
-              // TODO: new key to remove console error
-              return <li key={q}>{q}</li>;
-            })}
-          </ol>
-          <div className="sheetpreview__watermark">
-            <img
-              className="watermark__image"
-              src={require("../img/questation-logo.png")}
-              alt="Questation logo"
-            />
-          </div>
-        </div>
-      );
-    }
-    // answers
-    let arrToMap = ["4+4", "33/3"]; // this should be answersReducer
+    let arrToMap = Object.values(data);
     return (
       <div className="sheetpreview__container sheet">
         <h4 className="sheetpreview__sheet-number">
