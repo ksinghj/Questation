672bfd10d43df6dc3ade7d8f3e8d778b9c01dd10
ksinghj <kartarjabanda@hotmail.co.uk>
2020-05-26 17:47:02 +0100

    Update ActionButtons.jsx
    refactor ternerary operator jsx

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -3,33 +3,24 @@ import { Link } from "react-router-dom";
 import "../styles/ActionButtons/index.css";
 
 const ActionButtons = ({ isAnswers }) => {
-  if (!isAnswers) {
-    return (
-      <div className="action-buttons">
-        <button className="button whitespace-nw" type="button" disabled>
-          PDF view
-        </button>
-        <Link to="/answers" className="whitespace-nw">
-          <button className="button whitespace-nw" type="button">
-            Answer sheet
-          </button>
-        </Link>
-        <button className="button whitespace-nw" type="button">
-          Create new sheet
-        </button>
-      </div>
-    );
-  }
   return (
     <div className="action-buttons">
       <button className="button whitespace-nw" type="button" disabled>
         PDF view
       </button>
-      <Link to="/success" className="whitespace-nw">
-        <button className="button whitespace-nw" type="button">
-          Question sheets
-        </button>
-      </Link>
+      {isAnswers ? (
+        <Link to="/success" className="whitespace-nw">
+          <button className="button whitespace-nw" type="button">
+            Question sheets
+          </button>
+        </Link>
+      ) : (
+        <Link to="/answers" className="whitespace-nw">
+          <button className="button whitespace-nw" type="button">
+            Answer sheet
+          </button>
+        </Link>
+      )}
       <button className="button whitespace-nw" type="button">
         Create new sheet
       </button>
