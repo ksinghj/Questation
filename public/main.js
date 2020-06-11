a4f5494d2635faa31c0c18a0b43314a67d5085e7
ksinghj <kartarjabanda@hotmail.co.uk>
2020-06-11 11:20:33 +0100

    move main js

--- public/main.js
+++ public/main.js
@@ -11,7 +11,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  win.loadFile("./public/index.html");
+  win.loadFile("index.html");
 }
 
 app.whenReady().then(createWindow);
