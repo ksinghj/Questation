f3f1315a35d30198181f57df3ef2f3316273c680
ksinghj <kartarjabanda@hotmail.co.uk>
2020-07-15 22:33:31 +0100

    test replaceNums function replaces numbers with random numbers
    and does not replace other chars

--- jestTest.js
+++
@@ -1,6 +0,0 @@
-// this is to test Jest is working
-export const add = (num1, num2) => num1 + num2;
-
-// const add = (num1, num2) => num1 + num2;
-
-// module.exports = add;
--- src/components/alg.test.js
+++ src/components/alg.test.js
@@ -1,13 +1,19 @@
-// import { add } from "./alg";
+import { replaceNums } from "./alg";
 
-// describe("alg tests", () => {
-//   describe("replaceNums tests", () => {
-//     it("replaceNums replaces a number", () => {
-//       replaceNums();
-//       expect();
-//     });
-//   });
-// });
+describe("alg tests", () => {
+  describe("replaceNums tests", () => {
+    it("replaceNums replaces numbers - they should not be the same", () => {
+      let replace = "8 + 45 / 3";
+      let actual = replaceNums("8 + 45 / 3");
+      expect(actual).not.toBe(replace);
+    });
+    it("replaceNums does not replace other chars", () => {
+      let expected = "+-=/* a";
+      let actual = replaceNums("+-=/* a");
+      expect(actual).toEqual(expected);
+    });
+  });
+});
 
 // template
 // describe('', () => {
